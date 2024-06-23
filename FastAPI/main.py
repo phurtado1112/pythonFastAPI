c991a4e826532d23395d8b7e518cc098cabd56c5
Pablo Hurtado <administracione@phdsystems.net>
2024-06-22 22:49:12 -0600

    Se modifica el archivo basic_auth_users.py para sea accedido desde main.py como un acceso básico y se crea el archivo jwt_auth_users.py para ser accedido desde main.py como un acceso con token JWT

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,15 +1,22 @@
 from fastapi import FastAPI
-from FastAPI.routers import products, users
+from FastAPI.routers import products, users, basic_auth_users, jwt_auth_users
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
+# .venv\Scripts\activate acceso al entorno virtual
 # el servidor se inicia con: python -m uvicorn FastAPI.main:app --reload
 # El sitio se ejecuta en: http://127.0.0.1:8000
+# Documentación con Swagger: http://127.0.0.1:8000/docs
+# Documentación con Redocly: http://127.0.0.1:8000/redoc
 
 # Routers
 app.include_router(products.router)
 app.include_router(users.router)
+
+app.include_router(basic_auth_users.router)
+app.include_router(jwt_auth_users.router)
+
 app.mount('/static', StaticFiles(directory='FastAPI/static'), name='static')
 
 @app.get('/')
--- FastAPI/routers/jwt_auth_users.py
+++ FastAPI/routers/jwt_auth_users.py
@@ -1,12 +1,22 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from pydantic import BaseModel
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+
+# .venv\Scripts\activate acceso al entorno virtual
+# el servidor se inicia con: python -m uvicorn FastAPI.routers.jwt_auth_users:app --reload
+
+ALGORITHM = 'HS256'
+ACCESS_TOKEN_DURATION = 1
+SECRET = '950c00acbaace3a1f6ab4604b602e259bd56e651265b5ebfe58c3f0cc797defd'
 
 router = APIRouter()
 
 oauth2 = OAuth2PasswordBearer(tokenUrl='login')
 
-# el servidor se inicia con: python -m uvicorn FastAPI.routers.basic_auth_users:app --reload
+crypt = CryptContext(schemes=['bcrypt'])
 
 class User(BaseModel):
   username: str
@@ -25,40 +35,49 @@ users_db = {
       'full_name':'Pablo Hurtado', 
       'email':'phurtado1112@gmail.com', 
       'disabled':False,
-      'password':'123456'},
+      'password':'$2a$12$ez.S6p5Ry5GX4gA8JqrWLefNzTlsN5Go8n2ZQ9rHuZM/19W3BBHy.'
+      },
     'ihurtado': {
       'username':'ihurtado', 
       'full_name':'Isabel Hurtado', 
       'email':'ihurtado2412@gmail.com', 
       'disabled':True,
-      'password':'654321'}
+      'password':'$2a$12$J8UhxeW/sref03zat/rXR.N94esj/X2hj5fgcD7vjFaTS499IXg6G'
+      }
 }
 
 def search_user_db(username: str):
     if username in users_db:
         return UserDB(**users_db[username])
-
-
+    
 def search_user(username: str):
     if username in users_db:
         return User(**users_db[username])
-  
-async def current_user(token: str = Depends(oauth2)):
-    user = search_user(token)
-    if not user:
-        raise HTTPException(
+
+async def auth_user(token: str = Depends(oauth2)):
+  exception = HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Credenciales de autenticación inválidas",
             headers={"WWW-Authenticate": "Bearer"})
-
+  
+  try:
+    username = jwt.decode(token, SECRET, algorithms=[ALGORITHM]).get('sub')
+    if username is None:
+       raise exception
+  except JWTError:
+     raise exception
+  
+  return search_user(username)
+  
+async def current_user(user: User = Depends(auth_user)):
     if user.disabled:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Usuario inactivo")
 
     return user
-  
-@router.post('/loginb')
+    
+@router.post('/login')
 async def login(form: OAuth2PasswordRequestForm = Depends()):
     user_db = users_db.get(form.username)
     if not user_db:
@@ -66,12 +85,19 @@ async def login(form: OAuth2PasswordRequestForm = Depends()):
             status_code=status.HTTP_400_BAD_REQUEST, detail="El usuario no es correcto")
 
     user = search_user_db(form.username)
-    if not form.password == user.password:
+
+    if not crypt.verify(form.password, user.password):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail="La contraseña no es correcta")
+    
+    access_token_expiration = timedelta(minutes=ACCESS_TOKEN_DURATION)
 
-    return {"access_token": user.username, "token_type": "bearer"}
+    expire = datetime.utcnow() + access_token_expiration
 
-@router.get('/users/meb')
+    access_token = {'sub':user.username, 'exp':expire}
+
+    return {"access_token": jwt.encode(access_token, SECRET, algorithm=ALGORITHM), "token_type": "JWT"}
+
+@router.get('/users/me')
 async def me(user: User = Depends(current_user)):
   return user
